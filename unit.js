a1d163d661eed8c6eb777cd43fa7fbcb6ba2b689
bastienwcs <email>
2020-02-01 09:43:15 +0100

    init

---
+++ unit.js
@@ -0,0 +1,41 @@
+class Unit {
+
+    constructor() {
+        this._move = 0;
+        this._damage = 0;
+        this._reach = 0;
+        this._life = 0;
+    }
+
+    get move() {
+        return this._move;
+    }
+
+    set move(value) {
+        this._move = value;
+    }
+
+    get damage() {
+        return this._damage;
+    }
+
+    set damage(value) {
+        this._damage = value;
+    }
+
+    get reach() {
+        return this._reach;
+    }
+
+    set reach(value) {
+        this._reach = value;
+    }
+
+    get life() {
+        return this._life;
+    }
+
+    set life(value) {
+        this._life = value;
+    }
+}
