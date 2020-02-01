e49096fe99ffd29ac12338143345a0644c39282f
Antoine Zudas <mrgluptv@gmail.com>
2020-02-01 11:22:09 +0100

    c

--- dps.js
+++ dps.js
@@ -2,7 +2,7 @@ class Dps extends Unit {
 
     constructor(_posX,_posY) {
         super();
-        this._move = 1;
+        this._move = 3;
         this._damage = 4;
         this._reach = 2;
         this._life = 12;
