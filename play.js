0fcb9234145f857923173d5a92ddcc4d5d9925ef
bastienwcs <email>
2020-02-01 11:28:00 +0100

    shitty grid

--- play.js
+++ play.js
@@ -4,18 +4,28 @@ class Play {
 
         this._main = main;
 
-        let map = [
-            "      ",
-            "      ",
-            "      ",
-            "      ",
-            "      ",
-            "      "
-        ];
-
+        this._map = [];
+        for (let i = 0; i < 9; i++) {
+            this._map[i] = new Array(9);
+        }
     }
 }
 
-Play.prototype.play = function() {
-    console.log(this._main);
+Play.prototype.play = function () {
+
+    this._draw();
+};
+
+Play.prototype._draw = function () {
+    this._main.width = 800;
+    this._main.height = 600;
+    this._main.zIndex = 0;
+
+    for (let i = 0; i < this._map.length; i++) {
+        for (let j = 0; j < this._map[i].length; j++) {
+            const ctx = this._main.getContext('2d');
+            ctx.fillStyle = (i + j) % 2 === 0 ? 'green' : 'yellow';
+            ctx.fillRect(i * 30, j * 30, 30, 30);
+        }
+    }
 };
